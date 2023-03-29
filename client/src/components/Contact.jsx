9a485c2c3e2b061581387c3d5f05a37ab4b93446
Gisselle Garcia <gissellegar46@gmail.com>
2023-03-28 19:16:22 -0700

    Import Modalbutton into the Contact card so each card can have a modal for contact info

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -20,6 +20,7 @@ const Contact = ({student, toUpdate, toDelete}) => {
             <Card.Title>{student.firstname} {student.lastname} {student.email} {student.phonenumber} {student.notes} </Card.Title>
             <Button variant="outline-danger" onClick={()=>{onDelete(student)}} style={{padding: '0.6em', marginRight:'0.9em'}}><ioicons.IoTrash/></Button>
             <Button variant="outline-info" onClick={()=>{onUpdate(student)}} style={{padding: '0.6em'}}> <ioicons.IoSync/></Button>
+            <button>  <Modalbutton/>  </button>
             </Card.Body>
         </Card>
     )
--- client/src/components/Modalbutton.jsx
+++ client/src/components/Modalbutton.jsx
@@ -9,8 +9,6 @@ const Modalbutton = () =>  {
 
     }
 
-
-
     return (
         <div>
             <button onClick={contactButtonHandler}>Contact Info</button>

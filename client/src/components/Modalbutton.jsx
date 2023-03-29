da5541e955dcc1cb0ec7b2cd739971cc61592228
Gisselle Garcia <gissellegar46@gmail.com>
2023-03-28 19:14:57 -0700

    Pass the user's selection down so we can change the visibility of the modal.

--- client/src/components/Modalbutton.jsx
+++ client/src/components/Modalbutton.jsx
@@ -4,7 +4,8 @@ import { useState } from 'react';
 const Modalbutton = () =>  {
     const [show, setShow] = useState(false);
 
-    const contactButtonHandler = () => {
+    const contactButtonHandler = (event) => {
+        setShow(!show);
 
     }
 
@@ -13,8 +14,14 @@ const Modalbutton = () =>  {
     return (
         <div>
             <button onClick={contactButtonHandler}>Contact Info</button>
+            <Modal show={show}/>
         </div>
     )
 }
 
 export default Modalbutton;
+
+/*
+-when the button is clicked, we want to change the contact info to toggle if it is showing/not showing. 
+- here we pass the user's selection down so we can change the visibility of the modal.
+ */
